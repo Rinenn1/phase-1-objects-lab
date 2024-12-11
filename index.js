4e41c8589e00a51b9b9db51c84880ccbfb7791fc
Isaac <bennisaac24@gmail.com>
2024-12-11 13:43:22 +0300

    solved objects lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+//create an eployee variable with the name and streetAddress elements
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+
+    return obj;
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "12 Broadway");
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObject = {...obj};
+    delete newObject[key];
+    return newObject;
+}
+
+deleteFromEmployeeByKey(employee, "name");
+
+
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return  obj;
+}
+
+destructivelyDeleteFromEmployeeByKey(employee, "name");
